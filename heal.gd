1c9ef3780b20c9f1a744ed121296d8fe3d72e079
Sliner25 <unwichtig-90@gmx.de>
2025-04-22 20:30:57 +0200

    Neue Dateien hinzugefügt

---
+++ heal.gd
@@ -0,0 +1,29 @@
+extends Skill
+#hier das script von Skills noch importieren?
+
+var dragging := false
+var offset := Vector2.ZERO
+var dmg := -15 #negativer Wert für Heal
+var player
+
+func _ready():
+	add_to_group("Skill") #damit er alle Skills Aspekte hat
+	player = get_node("/root/Main/Player") #damit er mit dem Boss und seinen Funktionen interagieren kann
+	cooldown = 5  # Fireball braucht 3 Runden Cooldown
+	
+	
+func _input_event(viewport, event, shape_idx): #drag and drop
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			dragging = true
+			offset = get_global_mouse_position() - global_position
+		else:
+			dragging = false
+func use():
+	player.damage(dmg)
+	
+	
+	
+func _process(_delta):
+	if dragging:
+		global_position = get_global_mouse_position() - offset
