0866e246969a8a7ec68a110655a2a48a5773e05e
Sliner25 <unwichtig-90@gmx.de>
2025-06-11 19:15:44 +0200

    Zweihandkämpfer Skills angepasst.

--- Helden/Fighter/Skills/zerschmettern.gd
+++ Helden/Fighter/Skills/zerschmettern.gd
@@ -1,2 +1,8 @@
 extends Skill
 class_name zerschmettern
+
+func use():
+	GlobalVariables.bossArmor -=5
+	print("Bossrüstung zerstört. Neuer Rüstungswert des Boss: ", GlobalVariables.bossArmor)
+	if GlobalVariables.bossArmor <= 0:
+		GlobalVariables.bossArmor == 0
